e2f9a6cc7694f0166268c35674cd9f402e68ea66
kamalmistry6 <kamalmistry162@gmail.com>
2023-10-03 19:20:13 +0530

    new

--- script.js
+++ script.js
@@ -31,8 +31,55 @@ $(document).ready(function(){
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
-    });
-
-    
-    
+    });   
 });
+
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file;
+button.onclick = () => {
+input.click();
+}
+input.addEventListener("change", function() {
+file = this.files[0];
+dropArea.classList.add("active");
+showFile();
+});
+dropArea.addEventListener("dragover", (event) => {
+event.preventDefault();
+dropArea.classList.add("active");
+dragText.textContent = "Release to Upload File";
+});
+dropArea.addEventListener("dragleave", () => {
+dropArea.classList.remove("active");
+dragText.textContent = "Drag & Drop to Upload File";
+});
+dropArea.addEventListener("drop", (event) => {
+event.preventDefault();
+file = event.dataTransfer.files[0];
+showFile();
+});
+function showFile() {
+let fileType = file.type;
+let validExtensions = ["image/jpeg", "image/jpg", "image/png", "application/pdf", ".doc", ".docx", "video/mp4", ".xml", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+];
+console.log(fileType)
+if (validExtensions.includes(fileType)) {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        dropArea.classList.remove("active");
+        dragText.textContent = "The file is successfully uploaded."
+        setTimeout(() => {
+            dragText.textContent = "Drag & Drop to Upload File"
+        }, 2000)
+    }
+    fileReader.readAsDataURL(file);
+} else {
+    alert("This is not an valid file!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+}
+}
