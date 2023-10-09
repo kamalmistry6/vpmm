b91c5be5023661523844d9dc5c3249bd49a5fe8e
kamalmistry6 <kamalmistry162@gmail.com>
2023-10-09 22:43:50 +0530

    update

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ $(document).ready(function(){
     });   
 });
 
+// drag and drop script start
+
 const dropArea = document.querySelector(".drag-area"),
 dragText = dropArea.querySelector("header"),
 button = dropArea.querySelector("button"),
@@ -83,3 +85,50 @@ if (validExtensions.includes(fileType)) {
     dragText.textContent = "Drag & Drop to Upload File";
 }
 }
+// drag and drop script end
+
+
+// contact form script start
+
+function Send() {
+        
+    var name = document.getElementById("Name").value;
+    var email = document.getElementById("Email").value;
+    var contact = document.getElementById("Contact").value;
+    var sub = document.getElementById("subject").value;
+    var mess = document.getElementById("message").value;
+
+    var body = "Name: " + name + "<br/> Email:" + email + "<br/> Message:" + mess;
+
+    console.log(body);
+    Email.send({
+        SecureToken : "ca130070-a4da-43d2-a27a-39c6f196c4cf",
+        To: 'kamalmistry162@gmail.com',
+        From: "thepcdemo@gmail.com",
+        Subject: sub,
+        Body: body
+    }).then(
+        message => {
+            if (message == 'OK') {
+
+                swal("Successfull", "Your Data Successfull Received", "success");
+            }
+            else {
+
+                swal("Something Wrong", "Your Data is not Received", "error");
+            }
+        }
+    );
+
+}
+
+// script for refresh input data
+
+let btnClear = document.querySelector('.button');
+let inputs = document.querySelectorAll('input');
+
+btnClear.addEventListener('click', () => {
+    inputs.forEach(input =>  input.value = '');
+});
+
+// contact form script end
